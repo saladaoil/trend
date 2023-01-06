fab24ad5d2b70aff062ebe5fd39703adb3c0c166
siteiiryoubugaihinn <siteiiryoubugaihinn2@gmail.com>
2023-01-06 11:55:32 +0900

    JSに戻しました

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-//import MyPractice from './MyPractice';
+import MyPractice from './MyPractice';
 
 function App() {
-  return (
+ return (
     <div className="App">
       <h1>Reactの練習</h1>
       <MyPractice />
     </div>
-  );
+    );
 }
 
 export default App;
