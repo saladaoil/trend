b9bb273dc5bfcbe58a179a2c56ea52124c0b446a
siteiiryoubugaihinn <siteiiryoubugaihinn2@gmail.com>
2023-01-06 11:03:15 +0900

    TypeScriptに変更

---
+++ MyPractice.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyPractice extends React.Component {
+  render() {
+    return (
+      <div className="MyPractice">
+        <p>コンポーネントの練習です。</p>
+      </div>
+    );
+  }
+}
+
+export default MyPractice;
