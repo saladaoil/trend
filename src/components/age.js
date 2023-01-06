03438028494e813c3d5149bbbdd7fe26eabb800c
siteiiryoubugaihinn <siteiiryoubugaihinn2@gmail.com>
2023-01-06 09:45:27 +0900

    《コミットのコメント》

--- src/components/age.js
+++ src/components/age.js
@@ -1,3 +1,4 @@
+/* 
 import React from 'react'
 
 const age = () => {
@@ -8,6 +9,21 @@ const age = () => {
 
 //test
 
+
 export default age
 
 //質問2（年齢）
+*/
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reactの練習</h1>
+    </div>
+  );
+}
+
+export default App;
